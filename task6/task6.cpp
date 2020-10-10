30d0f5e21ff0053a8da18e27a3dc11ed64983c69
Ryabchenkov V. M <vultar1500@gmail.com>
2020-10-10 14:42:39 +0300

    remove MyAllocStrategy

--- task6/task6.cpp
+++ task6/task6.cpp
@@ -16,40 +16,26 @@ public:
 };
 
 
-template<typename T>
-class MyAllocStrategy {
-public:
-    MyAllocStrategy() {}
-    ~MyAllocStrategy() {}
-
-    void* allocate(std::size_t n) { return new T; }
-
-    void deallocate(T* p, std::size_t n) { delete p; }
-};
-
-
 template <class T, class E = T>
 struct MyAllocator {
     using value_type = T;
 
     using Traits = std::allocator_traits<MyAllocator<T, E>>;
 
-    MyAllocator() : my_alloc(new MyAllocStrategy<E>()) {}
+    MyAllocator() {}
 
-    MyAllocator(MyAllocStrategy<E>& my_alloc) : my_alloc(&my_alloc) {}
+    MyAllocator(MyAllocator<E>& my_alloc){}
 
-    template<class U, class M = U> 
-    MyAllocator(const MyAllocator<U, M>& other) : my_alloc(other.my_alloc) {}
+    template<class U, class M = U>
+    MyAllocator(const MyAllocator<U, M>& other) {}
 
-    T* allocate(std::size_t n) { 
-        return reinterpret_cast<T*>(my_alloc->allocate(n * sizeof(T)));
+    T* allocate(std::size_t n) {
+        return reinterpret_cast<T*>(new E);
     }
 
-    void deallocate(T* p, std::size_t n) { 
-        my_alloc->deallocate(reinterpret_cast<E*>(p), n);
+    void deallocate(T* p, std::size_t n) {
+        delete reinterpret_cast<E*>(p);
     }
-
-    MyAllocStrategy<E>* my_alloc;
 };
 
 
@@ -58,5 +44,6 @@ int main()
     MyAllocator<A> allocator;
     // auto sp = std::make_shared<A>();
     auto sp = std::allocate_shared<A> (allocator);
+
     return 0;
 }
